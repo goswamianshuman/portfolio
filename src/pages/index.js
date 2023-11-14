e79b4b2b6ed894d6736f52aaad9c2a6ce9145e5d
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-11-14 17:09:38 +0530

    fix: deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import { default as Home } from "./Home";
-import { default as About } from "./About";
+import { default as Home } from "./home";
+import { default as About } from "./about";
 import { default as Projects } from "./projects";
-import { default as Contact } from "./Contact";
+import { default as Contact } from "./contact";
 
 export { Home, About, Projects, Contact };
