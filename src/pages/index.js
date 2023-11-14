13424c6b2737ec6d82c7961e3f825ade2df91129
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-11-14 17:23:27 +0530

    fix: filename

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { default as Home } from "./Home";
+import { default as Home } from "./home";
 import { default as About } from "./about";
 import { default as Projects } from "./projects";
 import { default as Contact } from "./contact";
